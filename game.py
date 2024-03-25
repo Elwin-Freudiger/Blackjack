ac344d2e627b1be92b515fe1e19311aba04e6b1e
Elwin Freudiger <elwin.freudiger@gmail.com>
2024-03-25 11:40:09 +0100

    adding hand class

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
-import random
+import random as rand
 from math import *
+
+value = {"Ace": 11, "2": 2, "3": 3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "Jack":10, "Queen":10, "King":10}
 
 class Deck():
     def __init__(self, deck_num = 1):
@@ -12,8 +14,8 @@ class Deck():
         self.cards = [(card, category) for category in colors for card in all_values]
 
     def shuffle(self):
-        random.shuffle(self.cards)
-        self.cut = random.int((52*self.deck_num*0.25), (52*self.deck_num*0.75))
+        rand.shuffle(self.cards)
+        self.cut = rand.randint((52*self.deck_num*0.25), (52*self.deck_num*0.75))
     
     def reshuffle_needed(self):
         if len(self.cards) <= self.cut or len(self.cards)<1:
@@ -25,11 +27,20 @@ class Deck():
         card = self.cards.pop(0)
         return card
     
-test = Deck(1)
-test.deck_creation()
-test.shuffle()
-test.reshuffle_needed()
+class Cards():
+    def __init__(self, name, value=0):
+        self.name = name
+        self.value = value
+    
+    def __str__(self):
+        return "%s of %s" % (self.name[0], self.name[1])
 
-
-
-
+class Hand:
+    def __init__(self):
+        self.hand = []
+    
+    def value(self, hand):
+        value = 0
+    
+    def reset_hand(self):
+        self.hand = []
